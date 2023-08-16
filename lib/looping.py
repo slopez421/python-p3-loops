a9a53288367c332e9e1ae987e84c93af1ccda7fb
Shania Lopez <slopez@fandm.edu>
2023-08-16 18:24:28 -0400

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        if(i == 0):
+            return print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_int = [int * int for int in int_list]
+    return squared_int
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        print
+        if (i % 3 == 0 and i % 5 == 0):
+           print("FizzBuzz")
+        elif (i % 3 == 0):
+            print("Fizz")
+        elif(i % 5 == 0):
+            print("Buzz")
+        else: 
+            print(i)
